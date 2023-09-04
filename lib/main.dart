dc5622e6e73eea6a9d4c898ecef7599e5bcae9ca
OLADISEA <ladisea55@gmail.com>
2023-09-04 22:53:45 +0100

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:blog_app/views/login/bloc/login_bloc.dart';
+import 'package:blog_app/views/login/log_in.dart';
+import 'package:blog_app/views/signup/bloc/register_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    
+    return
+      //RepositoryProvider(
+      //create: (context)=> BlocRepository(),
+      //child:
+      MultiBlocProvider(
+          providers:[
+            BlocProvider(create: (context)=> RegisterBloc()),
+            BlocProvider(create: (context)=> LogInBloc())
+          ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const LogIn()
+        ),
+      //),
+    );
+  }
+}
+
