d672f0a3f66dcbaacab511b5143fd817f0eb5d08
OLADISEA <ladisea55@gmail.com>
2023-09-05 20:21:39 +0100

    SignUp method added

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -2,12 +2,12 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 class BlogRepository {
-  String baseUrl = 'https://schoolinka-test-production.up.railway.app/auth/login';
+  String baseUrl = 'https://schoolinka-test-production.up.railway.app/auth';
 
   Future<void> handleLogIn(String email, String password) async {
     try {
       final response = await post(
-        Uri.parse(baseUrl),
+        Uri.parse('$baseUrl/login'),
         body: {
           'email': email,
           'password': password,
@@ -17,11 +17,48 @@ class BlogRepository {
       if (response.statusCode == 200) {
         print('Login successful');
         final successJson = json.decode(response.body);
-        final successMessage = successJson['mesage'] as String;
+        final successMessage = successJson['message'] as String;
+        print('the success message is $successMessage');
+      } else if (response.statusCode == 400) {
+        // Unauthorized: This will handle invalid credentials.
+        print('account already exists');
+        final errorJson = json.decode(response.body);
+        final errorMessage = errorJson['message'] as String;
+        print('The error message is: $errorMessage');
+
+        throw Exception(errorMessage);
+      }else {
+        // This will handle other error scenarios
+        throw Exception(response.reasonPhrase);
+      }
+    } catch (e) {
+      throw Exception('Error occurred during login: $e');
+    }
+  }
+
+
+  Future<void> handleSignUp(String firstName, String lastName,String email, String password) async {
+    try {
+      final response = await post(
+        Uri.parse('$baseUrl/register'),
+        body: {
+          'firstName': firstName,
+          'lastName': lastName,
+          'email': email,
+          'password': password,
+        },
+      );
+
+      if (response.statusCode == 201) {
+        //print('Registration successful');
+        final successJson = json.decode(response.body);
+        final successMessage = successJson['message'] as String;
+        print('the success message is $successMessage');
       } else if (response.statusCode == 400) {
         // Unauthorized: This will handle invalid credentials.
         final errorJson = json.decode(response.body);
         final errorMessage = errorJson['message'] as String;
+        print('The error message is: $errorMessage');
         throw Exception(errorMessage);
       }else {
         // This will handle other error scenarios
