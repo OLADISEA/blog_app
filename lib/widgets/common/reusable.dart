dc6fc4926361c69eb45bce3f5cd35c21bb2a5f02
OLADISEA <ladisea55@gmail.com>
2023-09-14 20:28:14 +0100

    Made appropriate margin and padding to the UI

--- lib/widgets/common/reusable.dart
+++ lib/widgets/common/reusable.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 
 Widget reusableTextField(String text,void Function(String value)? func){
-  return TextField(
-    onChanged: (value)=> func?.call(value),
-    decoration: InputDecoration(
-      hintText: text,
+  return Container(
+    margin: const EdgeInsets.only(left: 20,right: 20),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.2),
+          spreadRadius: 1,
+          blurRadius: 2,
+          offset: const Offset(0, 1),
+        ),
+      ],
+    ),
+    child: TextField(
+      onChanged: (value)=> func?.call(value),
+      decoration: InputDecoration(
+        hintText: text,
+        border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15))
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15))
+        )
+      ),
     ),
   );
 }
 
 
-Widget reusableText(String text){
+Widget reusableText({required String text,double? size}){
   return Text(text,
     style: TextStyle(
-        fontSize: 15,
+        fontSize: size??15,
         fontWeight: FontWeight.bold,
         color: Colors.grey.withOpacity(0.5)
     ),
@@ -24,14 +44,14 @@ Widget generalButton(String text, VoidCallback? onTap){
   return GestureDetector(
     onTap: onTap,
     child: Container(
-      margin: EdgeInsets.only(top: 30),
-      padding: EdgeInsets.only(left: 12,right: 12,top: 10,bottom: 10),
+      margin: const EdgeInsets.only(top: 30),
+      padding: const EdgeInsets.only(left: 12,right: 12,top: 10,bottom: 10),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
         color: Colors.blue
       ),
       child: Text(text,
-      style: TextStyle(
+      style: const TextStyle(
         fontSize: 15
       ),),
     ),
